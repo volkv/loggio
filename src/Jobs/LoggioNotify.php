8576415600181cb507cd5b7b9709eccdc1944983
volkv <pvolkv@gmail.com>
2023-04-15 15:47:46 +0700

    wip

--- src/Jobs/LoggioNotify.php
+++ src/Jobs/LoggioNotify.php
@@ -14,19 +14,22 @@ class LoggioNotify
     public function handle()
     {
 
+        // Clean old entries
+        LoggioModel::where('date','<',now()->subDay()->format('Y-m-d'))->delete();
+
         $message = config('app.name').'@'.app()->environment().': '.PHP_EOL.PHP_EOL;
 
         $allSlugs = LoggioModel::select('activity_slug')->distinct()->pluck('activity_slug')->toArray();
 
 
-        $items = LoggioModel::where('date', now()->format('Y-m-d'))->pluck('count', 'activity_slug');
+        $entries = LoggioModel::where('date', now()->format('Y-m-d'))->pluck('count', 'activity_slug');
         $prevItems = LoggioModel::where('date', now()->subDay()->format('Y-m-d'))->pluck('count', 'activity_slug');
 
         foreach ($allSlugs as $slug) {
-            $items[$slug] = $items[$slug] ?? 0;
+            $entries[$slug] = $entries[$slug] ?? 0;
         }
 
-        foreach ($items as $slug => $count) {
+        foreach ($entries as $slug => $count) {
 
             $prevCount = $prevItems[$slug] ?? 0;
             $diff = $count - $prevCount;
